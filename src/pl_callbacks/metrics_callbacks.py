[bugfix] fix val acc by group computation

src/pl_callbacks/metrics_callbacks.py
@@ -39,7 +39,8 @@ class GroupAccuracyMonitor(Callback):
         correct = (class_labels == pred_labels).float()
 
         # Groupby: https://twitter.com/jeremyphoward/status/1185062637341593600
-        idxs, vals = torch.unique(group_labels, return_counts=True)
+        sorted_group_labels = group_labels.sort().values
+        idxs, vals = torch.unique(sorted_group_labels, sorted=True, return_counts=True)
         vs = torch.split_with_sizes(correct, tuple(vals))
         group_accs = {k.item(): v.mean().item() for k, v in zip(idxs, vs)}
         for group, acc in group_accs.items():