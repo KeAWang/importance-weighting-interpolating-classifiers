88cb35be7066a724d6e89560cb797e1cd8c06b5b
Alex Wang <kaw293@cornell.edu>
2021-07-22 23:09:38 -0700

    [scratch] add two gaussians notebook and update two-gaussians.py

--- scratch/two-gaussians.py
+++ scratch/two-gaussians.py
@@ -26,21 +26,25 @@ class PolyTailLoss(torch.nn.Module):
 
     def margin_fn(self, margin_vals):
         indicator = margin_vals <= 1
-        scores = torch.zeros_like(margin_vals)
         inv_part = torch.pow(margin_vals, -1 * self.alpha)
         logit_inner = -1 * margin_vals
         logit_part = (torch.log(torch.exp(logit_inner) + 1)) / math.log(
             1 + math.exp(-1)
         )
-        scores[indicator] = logit_part[indicator]
-        scores[~indicator] = inv_part[~indicator]
+        scores = logit_part * indicator + inv_part * (~indicator)
         return scores
 
 
 class_one_num = 100
 class_two_num = 5000
 loss_type = "poly"
+alpha = 1.0
 weight_type = "none"
+num_epochs = 10000
+batch_size = 32
+lr = 0.1
+device = "cuda"
+
 
 class_one = truncate_normal(torch.randn(class_one_num, 2), 6) + torch.tensor([[0, 6]])
 class_two = truncate_normal(torch.randn(class_two_num, 2), 6) + torch.tensor([[6, 0]])
@@ -49,38 +53,52 @@ x_seq = torch.cat((class_one, class_two), dim=0)
 y_seq = torch.cat(
     (torch.ones(class_one.shape[0]) * (-1), torch.ones(class_two.shape[0]))
 )
-if weight_type == "iw":
-    loss_ratio = (y_seq > 0) + (y_seq < 0) * class_two_num / float(class_one_num)
-else:
-    loss_ratio = torch.ones(y_seq.shape[0])
 
+dataset = torch.utils.data.TensorDataset(x_seq, y_seq)
+dataloader = torch.utils.data.DataLoader(
+    dataset=dataset, batch_size=batch_size, shuffle=True
+)
 
-lin_layer = torch.nn.Linear(2, 1)
+model = torch.nn.Linear(2, 1)
+model = model.to(device)
 
 if loss_type == "logit":
     logit_loss = torch.nn.SoftMarginLoss(reduction="none")
 else:
-    logit_loss = PolyTailLoss(alpha=5.0)
+    logit_loss = PolyTailLoss(alpha=alpha)
 
-optimizer = optim.SGD(lin_layer.parameters(), lr=1.0)
+optimizer = optim.SGD(model.parameters(), lr=0.1)
 
 w_seq = []
-for i in range(100000):
-    optimizer.zero_grad()
-    margin = lin_layer(x_seq).squeeze()
-    loss = torch.mean(logit_loss(margin, y_seq) * loss_ratio)
-    loss.backward()
-    optimizer.step()
+for i in range(num_epochs):
+    for xs, ys in dataloader:
+        xs = xs.to(device)
+        ys = ys.to(device)
+
+        optimizer.zero_grad()
+
+        margin = model(xs).squeeze()
+        if weight_type == "iw":
+            loss_ratio = (ys > 0) + (ys < 0) * class_two_num / float(class_one_num)
+        else:
+            loss_ratio = torch.ones(ys.shape[0])
+        loss_ratio = loss_ratio.to(device)
+        loss = torch.mean(logit_loss(margin, ys) * loss_ratio)
+
+        loss.backward()
+
+        optimizer.step()
     if i % 50 == 0:
-        print(loss.item())
+        print(f"Epoch {i}: loss={loss.item()}")
         w_seq.append(
-            [(n, p.detach().numpy().copy()) for n, p in lin_layer.named_parameters()]
+            [(n, p.detach().cpu().numpy().copy()) for n, p in model.named_parameters()]
         )
 
 print(w_seq)
 
+model = model.cpu()
 
-xrng = np.linspace(min(x_seq[:, 0]), max(x_seq[:, 0]), num=50)
+xrng = np.linspace(min(xs[:, 0]), max(xs[:, 0]), num=50)
 
 
 def ymap(xseq, weight, bias):
@@ -89,18 +107,18 @@ def ymap(xseq, weight, bias):
     return (xseq * weight[0] + bias) / (-1 * weight[1])
 
 
-plt.figure()
-plt.scatter(class_one[:, 0], class_one[:, 1])
-plt.scatter(class_two[:, 0], class_two[:, 1], color="red")
-cmap = plt.cm.get_cmap("cool")
+fig, ax = plt.subplots()
+ax.scatter(class_one[:, 0], class_one[:, 1])
+ax.scatter(class_two[:, 0], class_two[:, 1], color="red")
+cmap = plt.cm.get_cmap("BuPu")
 for widx in range(len(w_seq)):
     frac_val = float(widx) / float(len(w_seq))
     print(frac_val)
     yvals = ymap(xrng, w_seq[widx][0][1][0], w_seq[widx][1][1])
-    plt.plot(xrng, yvals, color=cmap(frac_val))
-plt.plot(xrng, xrng, color="green", linewidth=4)
-plt.ylim(min(x_seq[:, 1]), max(x_seq[:, 1]))
-plt.savefig(
+    ax.plot(xrng, yvals, color=cmap(frac_val))
+ax.plot(xrng, xrng, color="green", linewidth=4)
+ax.set(ylim=(min(xs[:, 1]), max(xs[:, 1])))
+fig.savefig(
     "tmp/"
     + loss_type
     + "-"
@@ -111,4 +129,3 @@ plt.savefig(
     + str(class_two_num)
     + ".png"
 )
-plt.close()
